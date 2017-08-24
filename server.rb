c9cf71586acd1cc872e44d315ffc45ba104f969b
Pedro De Ona <Pedro De Ona>
2017-08-24 18:45:45 -0400

    took method out of public/

--- server.rb
+++ server.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require_relative 'public/craigslist_scraper'
+require_relative 'craigslist_scraper'
 
 get '/' do
   File.open(File.join("public", "hello.html"))
@@ -15,9 +15,9 @@ get '/jobs' do
   url = "https://miami.craigslist.org/search/"
   url.<<(job) unless job.nil?
   unless url == "https://miami.craigslist.org/search/"
-    CraigslistScraper::Scraper.new(url).scrape(log_name)
+    CraigslistScraper.new(url).scrape(log_name)
   else
-    CraigslistScraper::Scraper.new.scrape(log_name)
+    CraigslistScraper.new.scrape(log_name)
   end
   send_file File.expand_path(log_name.<<('.html'), settings.root)
 end
