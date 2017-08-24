c54161c1bd2c74c089f98af21bedcda7d7c53654
Pedro De Ona <Pedro De Ona>
2017-08-24 10:40:52 -0400

    improved aesthetics of output list

--- public/craigslist_scraper.rb
+++ public/craigslist_scraper.rb
@@ -2,7 +2,6 @@ require 'nokogiri'
 require 'open-uri'
 
 module CraigslistScraper
-
   class Scraper
     attr_reader :url
     def initialize url="https://miami.craigslist.org/search/sof"
@@ -29,10 +28,9 @@ module CraigslistScraper
       # yy = time.year % 100
       scrape_log = File.new("job_log_today.html", "w+")
       dom.css('li[class=result-row]').each_with_index { |element, i| 
-        element = element.to_s
-        element.slice!("favorite this post").slice!(%(
-                <span class="restore-narrow-text">restore</span>
-                <span class="restore-wide-text">restore this posting</span>))
+        element.xpath("//*[@class='restore-link']").remove
+        element.xpath("//*[@class='result-meta']").remove
+        element = element.to_s.chomp
         element.gsub!('<a href="', '<a href="http://miami.craigslist.org')
         # puts element+"<br>"
         scrape_log.puts "#{i.+(1)}. "+element+"<br>"
